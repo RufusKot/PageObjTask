82ca2197db9b5680f81fba64586e3260ab7d6460
Denis Filitov <d_filitov@wargaming.net>
2021-08-09 18:39:47 +0300

    should_be_login_link implemented into BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser: WebDriver = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
     def open(self):
         self.browser.get(self.url)
