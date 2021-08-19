f7d30b98094e752eb72bc2a0be3785194d389711
Denis Filitov <d_filitov@wargaming.net>
2021-08-19 18:02:44 +0300

    Add to basket without assert made

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,6 @@ class MainPageLocators():
 class LoginPageLocators():
     login_form = (By.CSS_SELECTOR, '#id_login-username')
     register_form = (By.CSS_SELECTOR, '#id_registration-email')
+
+class ProductPageLocators():
+    add_to_basket_btn = (By.CSS_SELECTOR, '#add_to_basket_form button')
--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,12 +1,14 @@
 from .base_page import BasePage
-
+import time
+from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_to_basket(self):
-        add = self.browser.find_element_by_css_selector()
+        add = self.browser.find_element(*ProductPageLocators.add_to_basket_btn)
         add.click()
         self.solve_quiz_and_get_code()
+        time.sleep(40)
 
-    def check_if_in_basket(self):
-
-    def check_correct_price(self):
+    # def check_if_in_basket(self):
+    #
+    # def check_correct_price(self):
